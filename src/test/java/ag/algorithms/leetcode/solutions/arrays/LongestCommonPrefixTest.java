836fef3964e8054c9b1a4f98cebe11f75f450077
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-31 13:30:00 +0100

    edge case test

--- src/test/java/ag/algorithms/leetcode/solutions/arrays/LongestCommonPrefixTest.java
+++ src/test/java/ag/algorithms/leetcode/solutions/arrays/LongestCommonPrefixTest.java
@@ -14,4 +14,36 @@ class LongestCommonPrefixTest {
 
         Assertions.assertThat(result).isEqualTo("fl");
     }
+
+    @Test
+    void compute2() {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String result = longestCommonPrefix.compute(new String[]{"flower", "flowers", "flower"});
+
+        Assertions.assertThat(result).isEqualTo("flower");
+    }
+
+    @Test
+    void compute_no_common() {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String result = longestCommonPrefix.compute(new String[]{"xyz", "yz", "yzx"});
+
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    void compute_empty_list() {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String result = longestCommonPrefix.compute(new String[]{""});
+
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    void compute_null_list() {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String result = longestCommonPrefix.compute(null);
+
+        Assertions.assertThat(result).isEqualTo("");
+    }
 }
