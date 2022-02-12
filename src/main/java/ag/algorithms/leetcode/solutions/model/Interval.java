68ce7825e39c71e049d8795c8aaa9505966e8872
Attila Gyori <attila.gyori.id@gmail.com>
2022-02-12 09:46:26 +0100

    Meeting Rooms model

---
+++ src/main/java/ag/algorithms/leetcode/solutions/model/Interval.java
@@ -0,0 +1,28 @@
+package ag.algorithms.leetcode.solutions.model;
+
+public class Interval {
+
+    private Integer start;
+    private Integer end;
+
+    public Interval(Integer start, Integer end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public void setEnd(Integer end) {
+        this.end = end;
+    }
+}
