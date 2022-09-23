a67a1c372e6065ef3040af16f55cbf2beff36839
attilagyoriid <attila.gyori.id@gmail.com>
2022-09-23 09:04:10 +0200

    amend missing numbers with different approaches

--- src/main/java/ag/algorithms/leetcode/solutions/arrays/MissingNumber.java
+++ src/main/java/ag/algorithms/leetcode/solutions/arrays/MissingNumber.java
@@ -1,5 +1,10 @@
 package ag.algorithms.leetcode.solutions.arrays;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 public class MissingNumber {
 
     public int calculate(int[] items) {
@@ -10,5 +15,36 @@ public class MissingNumber {
         }
 
         return result;
+    }
+
+    public int getMissingNumberWithSet(List<Integer> items) {
+        Set<Integer> lookUpList = new HashSet();
+
+        IntStream.range(0, items.size() + 1).forEach(i -> lookUpList.add(i));
+
+        lookUpList.removeIf(i -> items.contains(i));
+        return lookUpList.stream().findFirst().get();
+
+    }
+
+    public int getMissingNumberWithXOR(List<Integer> items) {
+        int result = 0;
+        for (int i: items) {
+            result ^= i;
+        }
+        for (int i = 0; i <= items.size(); i++) {
+            result ^= i;
+        }
+
+        return result;
+
+    }
+
+    public int getMissingNumberWithSubtraction(List<Integer> items) {
+        int result = IntStream.range(0, items.size() + 1).sum();
+        int listSum = items.stream().mapToInt(Integer::valueOf)
+                .sum();
+        return result - listSum;
+
     }
 }
--- src/test/java/ag/algorithms/leetcode/solutions/arrays/MissingNumberTest.java
+++ src/test/java/ag/algorithms/leetcode/solutions/arrays/MissingNumberTest.java
@@ -2,6 +2,12 @@ package ag.algorithms.leetcode.solutions.arrays;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +18,33 @@ class MissingNumberTest {
 
         MissingNumber missingNumber = new MissingNumber();
         Assertions.assertThat(missingNumber.calculate(new int[]{0,1,3})).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideItems")
+    void getMissingNumber(List<Integer> items, int expected) {
+        MissingNumber missingNumber = new MissingNumber();
+        org.junit.jupiter.api.Assertions.assertEquals(expected,missingNumber.getMissingNumberWithSet(items));
+    }
+
+    private static Stream<Arguments> provideItems() {
+        return Stream.of(
+                Arguments.of(List.of(0, 3, 1), 2),
+                Arguments.of(List.of(0, 1), 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideItems")
+    void getMissingNumberWithXOR(List<Integer> items, int expected) {
+        MissingNumber missingNumber = new MissingNumber();
+        org.junit.jupiter.api.Assertions.assertEquals(expected,missingNumber.getMissingNumberWithXOR(items));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideItems")
+    void getMissingNumberWithSubtraction(List<Integer> items, int expected) {
+        MissingNumber missingNumber = new MissingNumber();
+        org.junit.jupiter.api.Assertions.assertEquals(expected,missingNumber.getMissingNumberWithSubtraction(items));
     }
 }
